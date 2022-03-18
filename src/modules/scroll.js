5067507473e6f60ea8cb899c5ef1d66b80364e76
Anna <usune@mail.ru>
2022-03-19 02:05:58 +0300

    Скролл

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,5 +1,19 @@
 const scroll = () => {
-  const one = "scroll";
+  const links = document.querySelectorAll(".top-menu a");
+
+  links.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
+
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  });
 };
 
 export { scroll };
