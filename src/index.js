780a2760589a21dba92abbc7e4555652ea17488d
Anna <usune@mail.ru>
2022-03-21 22:17:47 +0300

    the end

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { scroll } from "./modules/scroll";
 import { addDots } from "./modules/addDots";
 import { slider } from "./modules/slider";
 import { carousel } from "./modules/carousel";
+import { sendForm } from "./modules/sendForm";
+import { maskPhone } from "./modules/maskPhone";
 
 modal();
 accordeon();
@@ -11,3 +13,5 @@ scroll();
 addDots();
 slider();
 carousel();
+maskPhone("tel-mask");
+sendForm({ formId: ["form-callback"] });
